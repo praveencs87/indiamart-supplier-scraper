aee4f0b2c96d2737916f6718af6e761958bf9089
CoderBunch <praveencs87@gmail.com>
2026-07-03 09:34:39 +0530

    chore: enforce PPE, memory limits, and auto-abort

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+import { armKillSwitch, disarmKillSwitch } from './utils/timeoutManager.js';
 import { Actor } from 'apify';
 import { CheerioCrawler, log } from 'crawlee';
 
@@ -112,7 +113,9 @@ try {
     log.info(`Starting IndiaMART crawler for ${searchUrls.length} start URLs...`);
     
     await crawler.addRequests(searchUrls);
+    armKillSwitch(crawler);
     await crawler.run();
+    disarmKillSwitch();
 
     log.info(`🎉 Finished! Extracted ${totalLeadsExtracted} supplier leads.`);
 } catch (error) {
